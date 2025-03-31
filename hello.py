703316497dca3f00b2ad3125a7bd3ec33cf8b9ef
ElizabeteAkona <144365499+ElizabeteAkona@users.noreply.github.com>
2025-03-31 22:27:58 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+# TODO
+import cs50
+from cs50 import *
+
+# ievade = input("What is your name?\n")
+# ievade = sc50.get_string("What is your name?\n")
+ievade = get_string("What is your name?\n")
+print(f"hello, {ievade}")
